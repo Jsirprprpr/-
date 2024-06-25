6c9f32f775c06118fa7d14f33874bbfbb541866f
Jsirprprpr <1375049271@qq.com>
2024-06-25 19:28:46 +0800

    Add files via upload

---
+++ 个人网站/个人网站/js/js.js
@@ -0,0 +1,13 @@
+var index=0;
+ 
+function ChangeImg() {
+    index++;
+    var a=document.getElementsByClassName("img-slide");
+    if(index>=a.length) index=0;
+    for(var i=0;i<a.length;i++){
+        a[i].style.display='none';
+    }
+    a[index].style.display='block';
+}
+
+setInterval(ChangeImg,1500);
